8f5b92103cfaaeb3261bd5c8bf654d9d07738314
Dvizee <166751897+Dvizee@users.noreply.github.com>
2025-04-13 21:12:45 -0400

    Update gambling.asm
    Added casino sign thing, added money system, added return to main menu from slots, changed slots award amounts, added leave casino option

--- gambling.asm
+++ gambling.asm
@@ -6,45 +6,101 @@ INCLUDE Irvine32.inc
    slot2 BYTE ?
    slot3 BYTE ?
 
+   money DWORD 100
+
+   casinoLine1 BYTE "   _____           _____ _____ _   _  ____  ", 0
+   casinoLine2 BYTE "  / ____|   /\    / ____|_   _| \ | |/ __ \ ", 0
+   casinoLine3 BYTE " | |       /  \  | (___   | | |  \| | |  | |", 0
+   casinoLine4 BYTE " | |      / /\ \  \___ \  | | | . ` | |  | |", 0
+   casinoLine5 BYTE " | |____ / ____ \ ____) |_| |_| |\  | |__| |", 0
+   casinoLine6 BYTE "  \_____/_/    \_\_____/|_____|_| \_|\____/", 0
+
+   balMsg  BYTE "Balance: ", 0
    casMsg  BYTE "Choose Your Game:", 0
    game1   BYTE "Press S for: Slots", 0
    game2   BYTE "Press R for: Roulette", 0
    game3   BYTE "Press H for: Horse Betting", 0
+   endMsg  BYTE "Press L to leave Casino", 0
 
 
-   topMsg  BYTE "Press ENTER to Spin", 0
+   topMsg  BYTE "Press ENTER to Spin for $50", 0
    bar     BYTE "+-------------------------+", 0
    line    BYTE "|                         |", 0
    pay1    BYTE "0 - 0 points", 0
-   pay2    BYTE "$ - 500 points", 0
-   pay3    BYTE "7 - 700 points", 0
+   pay2    BYTE "$ - 25 points", 0
+   pay3    BYTE "7 - 40 points", 0
    score   BYTE "Your Score: ", 0
-   playAgainMsg BYTE "Play Again? (Play - P, Quit - Q): ", 0
+   playAgainMsg BYTE "Play Again? (Play - P, Return - R): ", 0
 
 .code
 main PROC
 
+BigLoop:
    call Clrscr
    call Randomize
 
-   ; Prints Main Menu UI
-   mov edx, OFFSET casMsg
+   ; Draws the casino sign
+   mov  eax, yellow
+   call SetTextColor
+
+   mov edx, OFFSET casinoLine1
+   mov ecx, 1
+   call GotoxyAtLine
+   call WriteString
+   mov edx, OFFSET casinoLine2
    mov ecx, 2
+   call GotoxyAtLine
+   call WriteString
+   mov edx, OFFSET casinoLine3
+   mov ecx, 3
+   call GotoxyAtLine
+   call WriteString
+   mov edx, OFFSET casinoLine4
+   mov ecx, 4
+   call GotoxyAtLine
+   call WriteString
+   mov edx, OFFSET casinoLine5
+   mov ecx, 5
+   call GotoxyAtLine
+   call WriteString
+   mov edx, OFFSET casinoLine6
+   mov ecx, 6
+   call GotoxyAtLine
+   call WriteString
+
+   mov  eax, white
+   call SetTextColor
+
+   ; Prints Main Menu UI
+   mov edx, OFFSET balMsg
+   mov ecx, 8
+   call GotoxyAtLine
+   call WriteString
+   mov eax, money
+   CALL WriteDec
+
+   mov edx, OFFSET casMsg
+   mov ecx, 10
    call GotoxyAtLine
    call WriteString
 
    mov edx, OFFSET game1
-   mov ecx, 4
+   mov ecx, 12
    call GotoxyAtLine
    call WriteString
 
    mov edx, OFFSET game2
-   mov ecx, 5
+   mov ecx, 13
    call GotoxyAtLine
    call WriteString
 
    mov edx, OFFSET game3
-   mov ecx, 6
+   mov ecx, 14
+   call GotoxyAtLine
+   call WriteString
+
+   mov edx, OFFSET endMsg
+   mov ecx, 16
    call GotoxyAtLine
    call WriteString
 
@@ -54,12 +110,17 @@ main PROC
       je SlotsLoop
       cmp al, 'S'      ; uppercase S
       je SlotsLoop
+      cmp al, 'l'
+      je exitCasino
+      cmp al, 'L'
+      je exitCasino
       jmp WaitForGame  ; ignore all other keys
 
 
 SlotsLoop:
 
    ; Prints header/ insturctions
+   sub money, 50
    call Clrscr
    mov edx, OFFSET topMsg
    mov ecx, 2
@@ -247,14 +308,14 @@ SlotsLoop:
    je AddDollarFirst
    jmp CalcSlot2 ; jumps to slot 2 if first slot is 0
 
-; if first slot is 7, add 700 to score
+; if first slot is 7, add 40 to score
 AddSevenFirst:
-   add eax, 700
+   add eax, 40
    jmp CalcSlot2
 
-; adds 500 to score if first slot is a $
+; adds 25 to score if first slot is a $
 AddDollarFirst:
-   add eax, 500
+   add eax, 25
    jmp CalcSlot2
 
 ; checks slot 2
@@ -267,11 +328,11 @@ CalcSlot2:
    jmp CalcSlot3
 
 AddSevenSecond:
-   add eax, 700
+   add eax, 40
    jmp CalcSlot3
 
 AddDollarSecond:
-   add eax, 500
+   add eax, 25
    jmp CalcSlot3
 
 ; checks slot 3
@@ -284,34 +345,44 @@ CalcSlot3:
    jmp PrintScore
 
 AddSevenThird:
-   add eax, 700
+   add eax, 40
    jmp PrintScore
 
 AddDollarThird:
-   add eax, 500
+   add eax, 25
 
 ; prints score
 PrintScore:
     call WriteDec
+    add money, eax
 
+   mov edx, OFFSET balMsg
+   mov ecx, 16
+   call GotoxyAtLine
+   call WriteString
+   mov eax, money
+   CALL WriteDec
 
    call Crlf
    call Crlf
 
    mov edx, OFFSET playAgainMsg
+   mov ecx, 18
+   call GotoxyAtLine
    call WriteString
+
 PlayAgainPrompt:
     call ReadChar
     cmp al, 50h ; P
     je SlotsLoop
     cmp al, 70h ; p
     je SlotsLoop
-    cmp al, 51h ; Q
-    je QuitGame
-    cmp al, 71h ; q
-    je QuitGame
+    cmp al, 52h ; R
+    je BigLoop
+    cmp al, 72h ; r
+    je BigLoop
     jmp PlayAgainPrompt
-QuitGame:
+ExitCasino:
     exit
 main ENDP
 
